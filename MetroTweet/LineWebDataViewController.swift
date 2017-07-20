2135f302a5da88143ebed6178de14360c09b69ea
Emi Clark <emiko.clark@gmail.com>
2017-07-20 16:48:27 -0400

    initial VCs

---
+++ MetroTweet/LineWebDataViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LineWebDataViewController.swift
+//  MetroTweet
+//
+//  Created by Avery Barrantes on 7/20/17.
+//  Copyright © 2017 Avery Barrantes. All rights reserved.
+//
+
+import UIKit
+import WebKit
+class LineWebDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MetroTweet/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  MetroTweet
+//
+//  Created by Emiko Clark on 7/20/17.
+//  Copyright © 2017 Avery Barrantes. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
